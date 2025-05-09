62bbebe4e622f3ecbd05ca7193e6b6f6aa19492a
[CLOUD4] 안창민 <chm20060@gmail.com>
2025-05-09 17:15:55 +0900

    feat: i-cross 기본 설정, 바디헤더 추가

--- src/main/java/an/inhaintegration/config/WebConfig.java
+++ src/main/java/an/inhaintegration/config/WebConfig.java
@@ -3,6 +3,7 @@ package an.inhaintegration.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
     // _method 파라미터를 읽어 실제 HTTP 메서드를 바꿔줌
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://ec2-13-209-198-107.ap-northeast-2.compute.amazonaws.com:8082") // 프론트엔드가 실행 중인 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
